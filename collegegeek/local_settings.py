79bf59ace837ca3fe645a58455963db26bbd27db
Ram Kishan Teli <ramkishanteli2@gmail.com>
2022-02-12 01:45:10 +0530

    Database update

--- collegegeek/local_settings.py
+++ collegegeek/local_settings.py
@@ -5,6 +5,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
