253af4dcc30418b9fa008dd61269feba22cee458
Ram Kishan Teli <ramkishanteli2@gmail.com>
2022-02-12 01:26:50 +0530

    Changes Done

--- collegegeek/settings.py
+++ collegegeek/settings.py
@@ -78,17 +78,8 @@ WSGI_APPLICATION = 'collegegeek.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-
-        'NAME': 'CollegeGeek',
-
-        'USER': 'postgres',
-
-        'PASSWORD': 'admin',
-
-        'HOST': '127.0.0.1',
-
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
